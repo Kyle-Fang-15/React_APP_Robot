7ce5037e73ff8d7ec012513010ef3a19d754cecf
Yuankan Fang <yuankanf@hotmail.com>
2018-03-13 00:46:38 -0700

    add robots

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card.js';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-ReactDOM.render(<Hello prop1={"Hello" +" This is the first try"}/>, document.getElementById('root'));
+import {robots} from './robots.js'
+ReactDOM.render(<div>
+	<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+	<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+	<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+	<Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+	</div>, document.getElementById('root'));
 registerServiceWorker();
